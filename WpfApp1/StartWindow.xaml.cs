1d5fa43ca31d020c9d33f8ff27042f5d4c0bf6b3
gabri <gabri@SMSI>
2023-04-06 17:42:21 +0200

    Add project files.

---
+++ WpfApp1/StartWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
